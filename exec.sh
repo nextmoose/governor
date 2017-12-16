1c05174e734e50fe232f7448c3d8ee54b41eea49
Emory Merryman <emory.merryman@gmail.com>
2017-12-16 02:20:33 +0000

    fix exec script

--- exec.sh
+++ exec.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-docker \
-    container \
-    run \
-    --interactive \
-    --tty \
-    --rm \
-    --env-file private.env \
-    --env UPSTREAM_ID_RSA="${UPSTREAM_ID_RSA}" \
-    --env ORIGIN_ID_RSA="${ORIGIN_ID_RSA}" \
-    --env REPORT_ID_RSA="${REPORT_ID_RSA}" \
-    --label expiry=$(($(date +%s)+60*60*24*7)) \
-    --env DISPLAY \
-    nextmoose/governor:scratch/58a9c972-9286-4de0-93ca-5edf2b9980b5
+docker image pull nextmoose/governor:scratch/58a9c972-9286-4de0-93ca-5edf2b9980b5 &&
+    docker \
+        container \
+        run \
+        --interactive \
+        --tty \
+        --rm \
+        --env-file private.env \
+        --env UPSTREAM_ID_RSA="${UPSTREAM_ID_RSA}" \
+        --env ORIGIN_ID_RSA="${ORIGIN_ID_RSA}" \
+        --env REPORT_ID_RSA="${REPORT_ID_RSA}" \
+        --label expiry=$(($(date +%s)+60*60*24*7)) \
+        --env DISPLAY \
+        nextmoose/governor:scratch/58a9c972-9286-4de0-93ca-5edf2b9980b5
