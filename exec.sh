c5d058b7ff3975d037e062163c8b0bc834288162
Emory Merryman <emory.merryman@gmail.com>
2017-12-16 01:02:52 +0000

    add exec file

---
+++ exec.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+docker \
+    container \
+    run \
+    --interactive \
+    --tty \
+    --rm \
+    --label expiry=$(($(date +%s)+60*60*24*7)) \
+    --env DISPLAY \
+    nextmoose/governor:scratch/58a9c972-9286-4de0-93ca-5edf2b9980b5
