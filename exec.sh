e9a5234a369d1cca1d7df59ed5703815770efeea
Emory Merryman <emory.merryman@gmail.com>
2017-12-18 13:03:37 +0000

    configure secret editor

--- exec.sh
+++ exec.sh
@@ -12,6 +12,10 @@ docker image pull nextmoose/governor:scratch_58a9c972-9286-4de0-93ca-5edf2b9980b
         --env UPSTREAM_ID_RSA="$(cat private/upstream_id_rsa)" \
         --env ORIGIN_ID_RSA="$(cat private/origin_id_rsa)" \
         --env REPORT_ID_RSA="$(cat private/report_id_rsa)" \
+        --env GPG_SECRET_KEY="$(cat private/gpg_secret_key)" \
+        --env GPG2_SECRET_KEY="$(cat private/gpg2_secret_key)" \
+        --env GPG_OWNER_TRUST="$(cat private/gpg_owner_trust)" \
+        --env GPG2_OWNER_TRUST="$(cat private/gpg2_owner_trust)" \
         --env EXPIRY=$(($(date +%s)+60*60*24*7)) \
         --label expiry=$(($(date +%s)+60*60*24*7)) \
         --env DISPLAY \
