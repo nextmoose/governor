2b8e01fbf76a60830fc8a766b5e2267409458976
Emory Merryman <emory.merryman@gmail.com>
2017-12-19 16:18:20 +0000

    integrate secret-viewer into image

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,16 @@ COPY docker-compose.yml entrypoint.sh secret-viewer.sh pre-commit.sh /opt/docker
 RUN \
     apk update && \
         apk upgrade && \
-        apk add --no-cache bash make git gnupg findutils && \
+        apk add --no-cache bash make git gnupg findutils tree && \
+        TEMP=$(mktemp -d) && \
+        cd ${TEMP} && \
+        git init && \
+        git remote add origin https://git.zx2c4.com/password-store && \
+        git fetch origin master && \
+        git checkout origin/master && \
+        make install && \
+        cd / && \
+        rm -rf ${TEMP} && \
         rm -rf /var/cache/apk/*
 WORKDIR /opt/docker
 ENTRYPOINT ["sh", "/opt/docker/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,28 @@
 #!/bin/sh
 
-docker-compose pull &&
+cleanup() {
+    docker-compose stop &&
+        docker-compose rm -fv
+} &&
+    trap cleanup EXIT &&
+    TEMP=$(mktemp -d) &&
+    echo "${GPG_SECRET_KEY}" > ${TEMP}/gpg_secret_key &&
+    gpg --batch --import ${TEMP}/gpg_secret_key &&
+    echo "${GPG_OWNER_TRUST}" > ${TEMP}/gpg_owner_trust &&
+    gpg --batch --import-ownertrust ${TEMP}/gpg_owner_trust &&
+    rm -rf ${TEMP} &&
+    docker-compose pull &&
     docker-compose up -d &&
-    sh &&
+    pass init ${GPG_KEY_ID} &&
+    pass git init &&
+    pass git config user.name "${USER_NAME}" &&
+    pass git config user.email "${USER_EMAIL}" &&
+    pass git remote add origin https://${SECRETS_HOST_NAME}:${SECRETS_HOST_PORT}/${SECRETS_ORIGIN_ORGANIZATION}/${SECRETS_ORIGIN_REPOSITORY}.git &&
+    pass git fetch origin master &&
+    pass git checkout origin/master &&
+    cp /opt/docker/pre-commit.sh ${HOME}/.password-store/.git/hooks/pre-commit &&
+    chmod 0500 ${HOME}/.password-store/.git/hooks/pre-commit &&
+    bash &&
     docker-compose stop &&
     docker-compose rm -fv
     
