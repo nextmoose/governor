1633a3573a2e70ba9fd49d3a3b1b9eade5fe5a96
Emory Merryman <emory.merryman@gmail.com>
2017-12-19 15:34:08 +0000

    add env vars and use them

--- secret-viewer.sh
+++ secret-viewer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-mkdir ${HOME}/opt &&
+apk update &&
+    apk upgrade &&
+    apk add --no-cache tree &&
+    mkdir ${HOME}/opt &&
     cd ${HOME}/opt &&
     git clone https://git.zx2c4.com/password-store &&
     cd ${HOME}/opt/password-store &&
@@ -9,7 +12,7 @@ mkdir ${HOME}/opt &&
     pass git init &&
     pass git config user.name "${USER_NAME}" &&
     pass git config user.email "${USER_EMAIL}" &&
-    pass git remote add origin https://${HOST_NAME}:${HOST_PORT}/${ORIGIN_ORGANIZATION}/${ORIGIN_REPOSITORY}.git &&
+    pass git remote add origin https://${SECRETS_HOST_NAME}:${SECRETS_HOST_PORT}/${SECRETS_ORIGIN_ORGANIZATION}/${SECRETS_ORIGIN_REPOSITORY}.git &&
     pass git fetch origin master &&
     pass git checkout origin/master &&
     cp /opt/docker/pre-commit.sh /home/user/.password-store/.git/hooks/pre-commit &&
