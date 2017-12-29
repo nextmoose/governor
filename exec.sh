369b98d8c7d060f111c7d77eabf56b1ebc46264b
Emory Merryman <emory.merryman@gmail.com>
2017-12-29 17:09:46 +0000

    wtf

--- exec.sh
+++ exec.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 docker(){
-    sudo docker run --interactive --tty --rm --label expiry=$(($(date +%s)+60*60*24*7)) --volume /var/run/docker.sock:/var/run/docker.sock:ro docker:17.10.
+    sudo docker run --interactive --tty --rm --label expiry=$(($(date +%s)+60*60*24*7)) --volume /var/run/docker.sock:/var/run/docker.sock:ro docker:17.12.0 "${@}"
 }
-sudo docker image pull nextmoose/governor:scratch_58a9c972-9286-4de0-93ca-5edf2b9980b5 &&
-    sudo docker \
+    docker image pull nextmoose/governor:scratch_fb54191d-78f2-4889-a85b-e4572dac6885 &&
+    docker \
         container \
         run \
         --interactive \
         --tty \
         --rm \
-        --env-file public.env \
+        --env USER_NAME="Emory Merryman" \
+        --env USER_EMAIL=emory.merryman@gmail.com
+        --env SECRETS_HOST_NAME=github.com
+        --env SECRETS_HOST_PORT=443
+        --env SECRETS_ORIGIN_ORGANIZATION=nextmoose
+        --env SECRETS_ORIGIN_REPOSITORY=secrets
+        --env GPG_KEY_ID=D65D3F8C
         --env GPG_SECRET_KEY="$(cat private/gpg_secret_key)" \
         --env GPG2_SECRET_KEY="$(cat private/gpg2_secret_key)" \
         --env GPG_OWNER_TRUST="$(cat private/gpg_owner_trust)" \
--- root/run.root.sh
+++ root/run.root.sh
@@ -4,4 +4,8 @@ dnf update --assumeyes &&
     dnf install --assumeyes install dnf-plugins-core &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest docker-compose &&
+    dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
+    adduser user &&
+    dnf install --assumeyes sudo &&
+    echo "user ALL=(ALL) NOPASSWD: /usr/bin/docker" > /etc/sudoers.d/user &&
     dnf clean all
