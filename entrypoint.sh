5a1feb705abca9e6cbc8931482e994183f19e8ad
Emory Merryman <emory.merryman@gmail.com>
2017-12-29 15:43:25 +0000

    temp rename of keys

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,28 +24,28 @@ cleanup() {
     do
         echo Please enter the correct decryption key to proceed.
     done &&
-    export UPSTREAM_ID_RSA=$(pass show ssh-keys/github/upstream/private) &&
-    export ORIGIN_ID_RSA=$(pass show ssh-keys/github/origin/private) &&
-    export REPORT_ID_RSA=$(pass show ssh-keys/github/report/private) &&
+    export UPSTREAM_ID_RSA=$(pass show ssh-keys.old/github/upstream/private) &&
+    export ORIGIN_ID_RSA=$(pass show ssh-keys.old/github/origin/private) &&
+    export REPORT_ID_RSA=$(pass show ssh-keys.old/github/report/private) &&
     export AWS_DEFAULT_REGION=$(pass show aws/aws-default-region) &&
     export AWS_ACCESS_KEY_ID=$(pass show aws/aws-access-key-id) &&
     export AWS_SECRET_ACCESS_KEY=$(pass show aws/aws-secret-access-key) &&
     echo SSH_KEYS AWS &&
-    export LIEUTENANT_AWS_PRIVATE_KEY=$(pass show aws/ssh-keys/lieutenant) &&
-    export LIEUTENANT_AWS_PUBLIC_KEY=$(sh /opt/docker/public-key.sh ssh-keys/lieutenant/raspberrypi "${LIEUTENANT_AWS_PRIVATE_KEY}") &&
+    export LIEUTENANT_AWS_PRIVATE_KEY=$(pass show aws/ssh-keys.old/lieutenant) &&
+    export LIEUTENANT_AWS_PUBLIC_KEY=$(sh /opt/docker/public-key.sh ssh-keys.old/lieutenant/raspberrypi "${LIEUTENANT_AWS_PRIVATE_KEY}") &&
     echo SSH_KEYS HOST &&
-    export LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys/lieutenant/ec2/public) &&
-    export LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys/lieutenant/ec2/private) &&
-    export VOLUMES_BACKUP_PUBLIC_KEY=$(pass show ssh-keys/volumes-backup/ec2/public) &&
-    export VOLUMES_BACKUP_PRIVATE_KEY=$(pass show ssh-keys/volumes-backup/ec2/private) &&
-    export HACKER_2_PAVILLION_PRIVATE_KEY=$(pass show ssh-keys/hp-pavillion/hacker/private) &&
-    export HACKER_2_PAVILLION_PUBLIC_KEY=$(pass show ssh-keys/hp-pavillion/hacker/public) &&
-    export HACKER_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys/lieutenant/hacker/private) &&
-    export HACKER_2_LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys/lieutenant/hacker/public) &&
-    export CHINESE_UBUNTU_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys/lieutenant/chinese-ubuntu/private) &&
-    export CHINESE_UBUNTU_2_LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys/lieutenant/chinese-ubuntu/public) &&
-    export RASPBERRYPI_UBUNTU_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys/lieutenant/raspberrypi/private) &&
-    export RASPBERRYPI_UBUNTU_2_LIEUTENANT_PUBLIC_KEY=$(sh /opt/docker/public-key.sh ssh-keys/lieutenant/raspberrypi) &&
+    export LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys.old/lieutenant/ec2/public) &&
+    export LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys.old/lieutenant/ec2/private) &&
+    export VOLUMES_BACKUP_PUBLIC_KEY=$(pass show ssh-keys.old/volumes-backup/ec2/public) &&
+    export VOLUMES_BACKUP_PRIVATE_KEY=$(pass show ssh-keys.old/volumes-backup/ec2/private) &&
+    export HACKER_2_PAVILLION_PRIVATE_KEY=$(pass show ssh-keys.old/hp-pavillion/hacker/private) &&
+    export HACKER_2_PAVILLION_PUBLIC_KEY=$(pass show ssh-keys.old/hp-pavillion/hacker/public) &&
+    export HACKER_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys.old/lieutenant/hacker/private) &&
+    export HACKER_2_LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys.old/lieutenant/hacker/public) &&
+    export CHINESE_UBUNTU_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys.old/lieutenant/chinese-ubuntu/private) &&
+    export CHINESE_UBUNTU_2_LIEUTENANT_PUBLIC_KEY=$(pass show ssh-keys.old/lieutenant/chinese-ubuntu/public) &&
+    export RASPBERRYPI_UBUNTU_2_LIEUTENANT_PRIVATE_KEY=$(pass show ssh-keys.old/lieutenant/raspberrypi/private) &&
+    export RASPBERRYPI_UBUNTU_2_LIEUTENANT_PUBLIC_KEY=$(sh /opt/docker/public-key.sh ssh-keys.old/lieutenant/raspberrypi) &&
     export SECRETS_ORIGIN_ORGANIZATION &&
     export SECRETS_ORIGIN_REPOSITORY &&
     docker-compose pull &&
