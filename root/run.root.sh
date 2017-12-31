1a2f2f3cc1606ad94d27d6775fdb0b6ab98d5896
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 16:59:22 +0000

    do not add user or sudo, use user.sudo

--- root/run.root.sh
+++ root/run.root.sh
@@ -5,7 +5,4 @@ dnf update --assumeyes &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest docker-compose &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
-    adduser user &&
-    dnf install --assumeyes sudo &&
-    echo "user ALL=(ALL) NOPASSWD: /usr/bin/docker" > /etc/sudoers.d/user &&
-    dnf clean all
+=   dnf clean all
