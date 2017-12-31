064c9f9a5f6854f45513a198df73576c839711b2
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 22:23:18 +0000

    a little bit

--- root/init.user.sh
+++ root/init.user.sh
@@ -13,6 +13,7 @@ TEMP=$(mktemp -d) &&
     pass git remote add origin https://${SECRETS_HOST_NAME}:${SECRETS_HOST_PORT}/${SECRETS_ORIGIN_ORGANIZATION}/${SECRETS_ORIGIN_REPOSITORY}.git &&
     pass git fetch origin master &&
     pass git checkout origin/master &&
-    ln -sf /usr/local/bin ${HOME}/.password-store/.git/hooks/pre-commit
+    ln -sf /usr/local/bin ${HOME}/.password-store/.git/hooks/pre-commit &&
+    cp /opt/docker/extension/docker-compose.yml /opt/docker/workspace
 
 
