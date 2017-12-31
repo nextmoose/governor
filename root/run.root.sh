b84eeeffb43f576b992fad2c28bd91abc16c65ac
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 17:05:50 +0000

    use fixed cloud9 image and fix = bug

--- root/run.root.sh
+++ root/run.root.sh
@@ -5,4 +5,4 @@ dnf update --assumeyes &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest docker-compose &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
-=   dnf clean all
+    dnf clean all
