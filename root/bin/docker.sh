f05eb72f7d27befd3bc7535b981902bc037af5dc
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 18:29:47 +0000

    bind mount docker.sock

--- root/bin/docker.sh
+++ root/bin/docker.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
-sudo /usr/bin/docker container run --interactive --tty --rm --label expiry=$(($(date +%s)+60*60*24*7)) docker:17.12.0 "${@}"
+sudo \
+    /usr/bin/docker \
+    container \
+    run \
+    --interactive \
+    --tty \
+    --rm \
+    --volume /var/run/docker.sock:/var/run/docker.sock:ro \
+    --label expiry=$(($(date +%s)+60*60*24*7)) \
+    docker:17.12.0 "${@}"
