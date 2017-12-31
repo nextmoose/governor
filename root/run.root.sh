174e8da760911b60ed8c9227a28209ec1ab3dd5b
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 23:42:39 +0000

    remove cruft

--- root/run.root.sh
+++ root/run.root.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dnf update --assumeyes &&
-    dnf install --assumeyes python2-pip psproc kill &&
+    dnf install --assumeyes python2-pip &&
     curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/sbin/docker-compose &&
     chmod 0500 /usr/local/sbin/docker-compose &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
