b223ce48c5604008c2b8df058a1c6ec81d007947
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 16:56:02 +0000

    fixed error in dnf install

--- root/run.root.sh
+++ root/run.root.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 dnf update --assumeyes &&
-    dnf install --assumeyes install dnf-plugins-core &&
+    dnf install --assumeyes dnf-plugins-core &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
     dnf install --assumeyes docker-common docker-latest docker-compose &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
