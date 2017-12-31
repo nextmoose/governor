86d4647b4deafd38b6ba57b28d1103ebfef012f4
Emory Merryman <emory.merryman@gmail.com>
2017-12-31 18:52:04 +0000

    fuck it

--- root/run.root.sh
+++ root/run.root.sh
@@ -3,6 +3,7 @@
 dnf update --assumeyes &&
     dnf install --assumeyes dnf-plugins-core &&
     dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo &&
-    dnf install --assumeyes docker-common docker-latest docker-compose &&
+    dnf install --assumeyes docker-common docker-latest python2-pip &&
+    curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose &&
     dnf install --assumeyes gnupg gnupg pass findutils bash-completion &&
     dnf clean all
